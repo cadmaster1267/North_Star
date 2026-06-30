68e48cfedc3f7541f013f95bf0d9f1e20fe00b28
cadmaster1267 <quadraticking@gmail.com>
2026-06-30 01:19:57 -0700

    Update email.js

--- email.js
+++ email.js
@@ -1,9 +1,16 @@
 function sendmail(){
-    const parms = {
+    var templateParams = {
         name : document.getElementById("name").value,
         note : document.getElementById("note").value,
 
     };
 
-    emailjs.send("service_8x0xogo", "template_c5avc09", parms) .then(alert("Your Nice Note is getting delivered!")),
+    emailjs.send('service_8x0xogo', 'template_c5avc09', templateParams).then(
+        (response) => {
+            alert("Your Nice Note is on its way!");
+        },
+        (error) => {
+            alert("Something went wrong, please try again or contact support.");
+        },
+    );
 }
