52abace1a73510343d80ae80c49ce5904126d923
cadmaster1267 <quadraticking@gmail.com>
2026-06-30 00:36:43 -0700

    Troubleshooting Nice notes

--- email.js
+++ email.js
@@ -1,9 +1,9 @@
 function sendmail(){
-    let parms = {
+    const parms = {
         name : document.getElementById("name").value,
         note : document.getElementById("note").value,
 
-    }
+    };
 
-    emailjs.send("service_8x0xogo","template_c5avc09",parms).then(alert("Your Nice Note is getting delivered!")),
+    emailjs.send("service_8x0xogo", "template_c5avc09", parms) .then(alert("Your Nice Note is getting delivered!")),
 }
